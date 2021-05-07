a6356091ddb8a8c99b6a23b6d3547a43fb181475
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-05-07 16:05:53 +0100

    additional styling

--- library/books/models.py
+++ library/books/models.py
@@ -12,6 +12,6 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length = 250)
     author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
-    borrower = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    borrower = models.ForeignKey(User, null=True,blank=True, on_delete=models.SET_NULL)
 
     
